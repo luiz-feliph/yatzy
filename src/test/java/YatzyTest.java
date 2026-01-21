a5a7efb755a135c7a8f2773a896ac5d6ca16233d
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-01-20 21:49:26 -0300

    Add failing scoring test for Yatzy category

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -10,4 +10,19 @@ public class YatzyTest {
     public void chance_returns_14_given_1_1_3_3_6() {
         assertEquals(14, yatzy.chance(1,1,3,3,6));
     }
+
+    @Test
+    public void chance_returns_21_given_4_5_5_6_1() {
+        assertEquals(21, yatzy.chance(4,5,5,6,1));
+    }
+
+    @Test
+    public void yatzy_returns_50_given_1_1_1_1_1() {
+        assertEquals(50, yatzy.yatzy(1,1,1,1,1));
+    }
+
+    @Test
+    public void yatzy_returns_0_given_1_1_1_2_1() {
+        assertEquals(50, yatzy.yatzy(1,1,1,1,1));
+    }
 }
