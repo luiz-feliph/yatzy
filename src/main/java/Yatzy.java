635bae2bdb6f7fd26e469cd9de66e9b1fa2f28e2
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-01-21 23:49:57 -0300

    Remove unnecessary code from Yatzy class

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -2,57 +2,7 @@ import java.util.*;
 
 public class Yatzy {
 
-    private int d1, d2, d3, d4, d5;
-
     public Yatzy() {
-    }
-
-    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
-        this.d1 = d1;
-        this.d2 = d2;
-        this.d3 = d3;
-        this.d4 = d4;
-        this.d5 = d5;
-    }
-
-    public int getD1() {
-        return d1;
-    }
-
-    public void setD1(int d1) {
-        this.d1 = d1;
-    }
-
-    public int getD2() {
-        return d2;
-    }
-
-    public void setD2(int d2) {
-        this.d2 = d2;
-    }
-
-    public int getD3() {
-        return d3;
-    }
-
-    public void setD3(int d3) {
-        this.d3 = d3;
-    }
-
-    public int getD4() {
-        return d4;
-    }
-
-    public void setD4(int d4) {
-        this.d4 = d4;
-    }
-
-    public int getD5() {
-        return d5;
-    }
-
-    public void setD5(int d5) {
-        this.d5 = d5;
     }
 
     public int chance(int d1, int d2, int d3, int d4, int d5) {
