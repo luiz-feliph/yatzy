762f33884182ced97504a00768f8e6c5bf4b0b8c
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-01-22 01:00:59 -0300

    Refactor all test names

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -8,228 +8,223 @@ public class YatzyTest {
 
     // Chance category
     @Test
-    public void chance_returns_14_given_1_1_3_3_6() {
+    public void chance_returns_sum_of_all_dice_values() {
         assertEquals(14, yatzy.chance(1, 1, 3, 3, 6));
     }
 
     @Test
-    public void chance_returns_21_given_4_5_5_6_1() {
+    public void chance_also_returns_sum_of_all_dice_values() {
         assertEquals(21, yatzy.chance(4, 5, 5, 6, 1));
     }
 
     // Yatzy category
     @Test
-    public void yatzy_returns_50_given_1_1_1_1_1() {
+    public void yatzy_returns_50_when_all_dice_values_are_equals() {
         assertEquals(50, yatzy.yatzy(1, 1, 1, 1, 1));
     }
 
     @Test
-    public void yatzy_returns_0_given_1_1_1_2_1() {
+    public void yatzy_returns_0_when_there_is_any_different_value() {
         assertEquals(0, yatzy.yatzy(1, 1, 1, 2, 1));
     }
 
     // Ones category
     @Test
-    public void ones_returns_3_given_1_2_1_6_1() {
+    public void ones_returns_sum_of_all_dice_with_face_value_one() {
         int face = 1;
         assertEquals(3, yatzy.sumDiceWithFace(face, 1, 2, 1, 6, 1));
     }
 
     @Test
-    public void ones_returns_0_given_3_3_3_4_5() {
+    public void ones_returns_0_when_there_are_no_dice_with_face_value_one() {
         int face = 1;
         assertEquals(0, yatzy.sumDiceWithFace(face, 3, 3, 3, 4, 5));
     }
 
     // Twos category
     @Test
-    public void twos_returns_8_given_2_2_1_2_2() {
+    public void twos_returns_sum_of_all_dice_with_face_value_two() {
         int face = 2;
         assertEquals(8, yatzy.sumDiceWithFace(face, 2, 2, 1, 2, 2));
     }
 
     @Test
-    public void twos_returns_0_given_1_3_6_4_5() {
+    public void twos_returns_0_when_there_are_no_dice_with_face_value_two() {
         int face = 2;
         assertEquals(0, yatzy.sumDiceWithFace(face, 1, 3, 6, 4, 5));
     }
 
     // Threes category
     @Test
-    public void threes_returns_6_given_2_3_1_3_2() {
+    public void threes_returns_sum_of_all_dice_with_face_value_three() {
         int face = 3;
         assertEquals(6, yatzy.sumDiceWithFace(face, 2, 3, 1, 3, 2));
     }
 
     @Test
-    public void threes_returns_0_given_1_5_2_4_1() {
+    public void threes_returns_0_when_there_are_no_dice_with_face_value_three() {
         int face = 3;
         assertEquals(0, yatzy.sumDiceWithFace(face, 1, 5, 2, 4, 1));
     }
 
     // Fours category
     @Test
-    public void fours_returns_4_given_4_3_5_6_2() {
+    public void fours_returns_sum_of_all_dice_with_face_value_four() {
         int face = 4;
         assertEquals(4, yatzy.sumDiceWithFace(face, 4, 3, 5, 6, 2));
     }
 
     @Test
-    public void fours_returns_0_given_6_5_2_3_3() {
+    public void fours_returns_0_when_there_are_no_dice_with_face_value_four() {
         int face = 4;
         assertEquals(0, yatzy.sumDiceWithFace(face, 6, 5, 2, 3, 3));
     }
 
     // Fives category
     @Test
-    public void fives_returns_20_given_4_5_5_5_5() {
+    public void fives_returns_sum_of_all_dice_with_face_value_five() {
         int face = 5;
         assertEquals(20, yatzy.sumDiceWithFace(face, 4, 5, 5, 5, 5));
     }
 
     @Test
-    public void fives_returns_0_given_1_1_2_3_3() {
+    public void fives_returns_0_when_there_are_no_dice_with_face_value_five() {
         int face = 5;
         assertEquals(0, yatzy.sumDiceWithFace(face, 1, 1, 2, 3, 3));
     }
 
     // Sixes category
     @Test
-    public void sixes_returns_6_given_6_1_2_4_5() {
+    public void sixes_returns_sum_of_all_dice_with_face_value_six() {
         int face = 6;
         assertEquals(6, yatzy.sumDiceWithFace(face, 6, 1, 2, 4, 5));
     }
 
     @Test
-    public void sixes_returns_0_given_4_1_2_5_3() {
+    public void sixes_returns_0_when_there_are_no_dice_with_face_value_six() {
         int face = 6;
         assertEquals(0, yatzy.sumDiceWithFace(face, 4, 1, 2, 5, 3));
     }
 
     // Pair category
     @Test
-    public void pair_returns_0_given_1_2_3_4_5() {
+    public void pair_returns_0_when_there_are_no_pairs() {
         assertEquals(0, yatzy.pair(1, 2, 3, 4, 5));
     }
 
     @Test
-    public void pair_returns_8_given_3_3_3_4_4() {
+    public void pair_returns_twice_the_highest_pair_value() {
         assertEquals(8, yatzy.pair(3, 3, 3, 4, 4));
     }
 
     @Test
-    public void pair_returns_12_given_1_1_6_2_6() {
+    public void pair_also_returns_twice_the_highest_pair_value() {
         assertEquals(12, yatzy.pair(1, 1, 6, 2, 6));
     }
 
     @Test
-    public void pair_returns_6_given_3_3_3_4_1() {
-        assertEquals(6, yatzy.pair(3, 3, 3, 4, 1));
-    }
-
-    @Test
-    public void pair_returns_6_given_3_3_3_3_1() {
+    public void pair_returns_only_twice_the_highest_pair_value_even_if_there_are_two_same_pairs() {
         assertEquals(6, yatzy.pair(3, 3, 3, 3, 1));
     }
 
     // Two pairs category
     @Test
-    public void two_pairs_returns_8_given_1_1_2_3_3() {
+    public void two_pairs_returns_sum_of_two_distinct_pairs() {
         assertEquals(8, yatzy.twoPairs(1, 1, 2, 3, 3));
     }
 
     @Test
-    public void two_pairs_returns_0_given_1_1_2_3_4() {
+    public void two_pairs_returns_0_when_there_is_only_one_pair() {
         assertEquals(0, yatzy.twoPairs(1, 1, 2, 3, 4));
     }
 
     @Test
-    public void two_pairs_returns_6_given_1_1_2_2_2() {
+    public void two_pairs_returns_sum_of_two_distinct_pairs_even_when_there_is_a_three_of_a_kind() {
         assertEquals(6, yatzy.twoPairs(1, 1, 2, 2, 2));
     }
 
     @Test
-    public void two_pairs_returns_0_given_3_3_3_3_1() {
+    public void two_pairs_returns_0_when_there_is_a_four_of_a_kind() {
         assertEquals(0, yatzy.twoPairs(3, 3, 3, 3, 1));
     }
 
     // Three of a kind category
     @Test
-    public void three_of_a_kind_returns_9_given_3_3_3_4_5() {
+    public void three_of_a_kind_returns_sum_of_three_equal_values() {
         assertEquals(9, yatzy.three_of_a_kind(3, 3, 3, 4, 5));
     }
 
     @Test
-    public void three_of_a_kind_returns_0_given_3_3_4_5_6() {
+    public void three_of_a_kind_returns_0_when_there_is_no_three_of_a_kind() {
         assertEquals(0, yatzy.three_of_a_kind(3, 3, 4, 5, 6));
     }
 
     @Test
-    public void three_of_a_kind_returns_9_given_3_3_3_3_1() {
+    public void three_of_a_kind_returns_sum_of_three_even_if_there_is_a_four_of_a_kind() {
         assertEquals(9, yatzy.three_of_a_kind(3, 3, 3, 3, 1));
     }
 
     // Four of a kind category
     @Test
-    public void four_of_a_kind_returns_8_given_2_2_2_2_5() {
+    public void four_of_a_kind_returns_sum_of_four_equal_values() {
         assertEquals(8, yatzy.four_of_a_kind(2, 2, 2, 2, 5));
     }
 
     @Test
-    public void four_of_a_kind_returns_0_given_2_2_2_5_5() {
+    public void four_of_a_kind_returns_0_when_there_is_no_four_of_a_kind() {
         assertEquals(0, yatzy.four_of_a_kind(2, 2, 2, 5, 5));
     }
 
     @Test
-    public void four_of_a_kind_returns_8_given_2_2_2_2_2() {
+    public void four_of_a_kind_returns_sum_of_four_when_all_dice_are_equal() {
         assertEquals(8, yatzy.four_of_a_kind(2, 2, 2, 2, 2));
     }
 
     // Small straight category
     @Test
-    public void small_straight_returns_15_given_1_2_3_4_5() {
+    public void small_straight_returns_sum_of_all_dice_when_there_are_all_numbers_from_1_to_5() {
         assertEquals(15, yatzy.smallStraight(1, 2, 3, 4, 5));
     }
 
     @Test
-    public void small_straight_returns_15_given_5_4_3_2_1() {
+    public void small_straight_returns_sum_of_all_dice_when_there_are_all_numbers_from_1_to_5_in_any_order() {
         assertEquals(15, yatzy.smallStraight(5, 4, 3, 2, 1));
     }
 
     @Test
-    public void small_straight_returns_0_given_2_3_4_5_6() {
+    public void small_straight_returns_0_when_there_are_not_all_numbers_from_1_to_5() {
         assertEquals(0, yatzy.smallStraight(2, 3, 4, 5, 6));
     }
 
     // Large straight category
     @Test
-    public void large_straight_returns_20_given_2_3_4_5_6() {
+    public void large_straight_returns_sum_of_all_dice_when_there_are_all_numbers_from_2_to_6() {
         assertEquals(20, yatzy.largeStraight(2, 3, 4, 5, 6));
     }
 
     @Test
-    public void large_straight_returns_20_given_6_5_4_3_2() {
+    public void large_straight_returns_sum_of_all_dice_when_there_are_all_numbers_from_2_to_6_in_any_order() {
         assertEquals(20, yatzy.largeStraight(6, 5, 4, 3, 2));
     }
 
     @Test
-    public void large_straight_returns_0_given_2_3_4_5_1() {
+    public void large_straight_returns_0_when_there_are_not_all_numbers_from_2_to_6() {
         assertEquals(0, yatzy.largeStraight(2, 3, 4, 5, 1));
     }
 
     // Full house category
     @Test
-    public void full_house_returns_8_given_1_1_2_2_2() {
+    public void full_house_returns_sum_of_all_dice_when_there_are_a_pair_and_a_three_of_a_kind() {
         assertEquals(8, yatzy.fullHouse(1, 1, 2, 2, 2));
     }
 
     @Test
-    public void full_house_returns_0_given_2_2_3_3_4() {
+    public void full_house_returns_0_when_there_are_no_pair_and_three_of_a_kind() {
         assertEquals(0, yatzy.fullHouse(2, 2, 3, 3, 4));
     }
 
     @Test
-    public void full_house_returns_0_given_4_4_4_4_4() {
+    public void full_house_returns_0_when_all_dice_values_are_equals() {
         assertEquals(0, yatzy.fullHouse(4, 4, 4, 4, 4));
     }
 }
